9ca082011e2ed05cf0824c750e6e0d88084afb44
Pranali <pranalilambat00@gmail.com>
2022-01-09 20:48:31 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasiccodeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Username.Pranali();
+            //LeapYear.Input();
+            //Powerof.Power();
+            //FlipCoin.TossTheCoin();
+            //Harmonic.NthHarmonic();
+            //Factors.PrimeFactor();
+            //presentAbsent.swapNum();
+            //QueRem.Remainder();
+            //EvenOdd.Eodd();
+            LargestNumber.LNum();
+            //vowel.checkForVowel();
+        }
+    }
+}
